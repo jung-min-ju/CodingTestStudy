ebd23dc1feced110223ef78464e952f28dfd412a
minju <minju9711@naver.com>
2023-11-22 01:19:57 +0900

    [feat] 두 번째 풀이[성공] - 시간복잡도 O(n)

--- baekjoon/b_12933/src/Main.java
+++ baekjoon/b_12933/src/Main.java
@@ -1,71 +1,64 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.HashMap;
 
 public class Main {
-    static List<QuackDto> QuackRepository = new ArrayList<>();
-    static int QuackNum = 0;
-    static boolean WrongInput = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Character, Integer> quackIndex = new HashMap<>();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        char[] input = br.readLine().toCharArray();
+        int[] checkQuack = new int[6];
+        int leastQuack = 0;
+        int minus = 0;
 
-        String inputString =  scanner.nextLine();
-        String [] InputArray = inputString.split("");
+        quackIndex.put('q',1);
+        quackIndex.put('u',2);
+        quackIndex.put('a',3);
+        quackIndex.put('c',4);
+        quackIndex.put('k',5);
 
-        for(String input : InputArray){
-            if(input.equals("q")){
-                if(QuackNum==0){ //현재까지 다 운 오리가 없을때만 새로운 오리 객체 생성
-                    QuackRepository.add(new QuackDto());
-                }
-                continue;
-            }
-            try{
-                CheckRepository(input);
-            }
-            catch (IllegalArgumentException e){
-                WrongInput = true;
+        checkQuack[0]=input.length;
+
+        if(input.length%5!=0) { //문자열의 길이가 5의 배수가 아닌경우, 프로그램 종료
+            leastQuack = -1;
+            System.out.println(leastQuack);
+            br.close();
+            return;
+        }
+
+        for(char quack : input){
+            int index = 0;
+            if(!quackIndex.containsKey(quack)){
+                leastQuack=-1;
                 break;
             }
-        }
-        if(QuackNum==0 || WrongInput){
-            QuackNum=-1;
-        }
-        System.out.println(QuackNum);
-    }
-
-    private static void CheckRepository(String inputString) {
-
-        boolean IsValidate = false;
-        for(QuackDto quackDto : QuackRepository){
-            int index = ToIndex(inputString);
-            boolean IsAdd = quackDto.AddQuack(index);
-            if(IsAdd) { //해당 dto에서 값을 넣었을때, quack 문자열 완성 시
-                if(quackDto.IsMax().equals("Max")){
-                    QuackNum++;
-                }
-                IsValidate = true;
+            index = quackIndex.get(quack);
+            checkQuack[index]++; //해당 인덱스에 ++를 해줌. 해당 문자열이 입력되었다는 의미를 지님
+            if(!(checkQuack[index-1]>=checkQuack[index])){ //이전 문자열의 갯수>=현재 문자열의 갯수 여야 함(순서는 q,u,a,c,k순임)
+                leastQuack = -1;
                 break;
             }
+            if(quack=='k'){ //k가 등장할때
+                if(checkQuack[1]>checkQuack[5] && minus==0){ //q>k && minus = 0
+                    if(leastQuack==0){ //최소 오리수가 안세어졌을때, 해당 케이스는 일단 quack 하나가 완성된 거니 최소 오리수++
+                        leastQuack++;
+                    }
+                    minus=checkQuack[1]-checkQuack[5]; //minjus=q-k
+                }
+                if(checkQuack[1]-checkQuack[5]>minus){ //k가 들어왓는데, q-k가 기존 minus보다 크다면 새로운 오리가 등장한 것!
+                    minus=checkQuack[1]-checkQuack[5];
+                }
+                if(checkQuack[1]==checkQuack[5] && leastQuack==0){ //q=k && 최소 오리수 안 세어진 경우
+                    leastQuack++;
+                }
+            }
         }
-        if(!IsValidate){
-            throw new IllegalArgumentException(); //inputString이 어느 객체에도 들어가지 않는다면, 잘못된 입력임
-        }
-    }
-
-    private static int ToIndex(String input){
-        if(input.equals("u")){
-            return 1;
-        }
-        else if(input.equals("a")){
-            return 2;
-        }
-        else if(input.equals("c")){
-            return 3;
+        if(checkQuack[5]*5!=input.length){ //모든 quack이 k까지 잘 입력되었는지 확인
+            leastQuack = -1;
         }
         else {
-            return 4;
+            leastQuack+=minus;
         }
+        System.out.println(leastQuack);
     }
-
 }
